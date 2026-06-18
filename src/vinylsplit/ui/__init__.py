97bc300404ddc91286904e86622e22a050ea453b
Pasleyjb <pasleyjb@gmail.com>
2026-06-18 07:16:17 -0500

    Add UI facade

--- src/vinylsplit/ui/__init__.py
+++ src/vinylsplit/ui/__init__.py
@@ -1,12 +1,39 @@
 from .banner import show_banner
 from .console import console
-from .output import error, info, success, warning
+from .tables import (
+    album_table,
+    audio_info_table,
+    track_table,
+)
 
-__all__ = [
-    "console",
-    "show_banner",
-    "info",
-    "success",
-    "warning",
-    "error",
-]
+
+class UI:
+    def banner(self, version: str):
+        show_banner(version)
+
+    def print(self, obj):
+        console.print(obj)
+
+    def info(self, text: str):
+        console.print(f"[cyan]ℹ[/] {text}")
+
+    def success(self, text: str):
+        console.print(f"[green]✓[/] {text}")
+
+    def warning(self, text: str):
+        console.print(f"[yellow]⚠[/] {text}")
+
+    def error(self, text: str):
+        console.print(f"[red]✗[/] {text}")
+
+    def audio_info(self, info):
+        console.print(audio_info_table(info))
+
+    def album(self, match):
+        console.print(album_table(match))
+
+    def tracks(self, tracks):
+        console.print(track_table(tracks))
+
+
+ui = UI()
